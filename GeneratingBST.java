50a5e9424f195e61aec0f21fa99aa9fa3c95fbb7
rheeshabh kumar singh <rishabhs861@gmail.com>
2020-09-25 18:29:09 +0530

    added sorting and swap function

--- GeneratingBST.java
+++ GeneratingBST.java
@@ -1,9 +1,34 @@
 package packageBST;
 
 public class GeneratingBST {
-
-	public void preorder(int[] a)
+	
+	static int[] inorder;
+	public void preorder(int[] postorder)
 	{
+		int n= postorder.length;
+		inorder=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			inorder[i]=postorder[i];
+		}
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				if(inorder[i]>inorder[j])
+					swap(i,j);
+			}
+		}
 		
 	}
+	
+	
+	static void swap(int i,int j)
+	{
+		int temp=inorder[i];
+		inorder[i]=inorder[j];
+		inorder[j]=temp;
+	}
+	
 }
